d64babe7db716257d9d5e3ef888054afd6ea21bc
Henry <herrya1010@gmail.com>
2026-03-12 12:34:25 +0700

    Remove .next directory from git tracking

--- .next/dev/static/chunks/src_app_[locale]_layout_tsx_643a1099._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules__pnpm_56cd49b3._.js",
-  "static/chunks/[root-of-the-server]__0f0ba101._.css"
-],
-    source: "dynamic"
-});
